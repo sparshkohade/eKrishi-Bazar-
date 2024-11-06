0296ec5ec8a72ae848b89cf36af263b72fae8c8d
sparshkohade <sparshkohade588@gmail.com>
2024-11-06 15:00:18 +0530

    all files added

---
+++ Backend/db.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config({ path: "./.env" });
+
+const mongoURI = `${process.env.MONGO_DB_URL}`;
+console.log(process.env.MONGO_DB_URL);
+
+const connectToMongo = () => {
+  mongoose.connect(mongoURI).then(console.log("connected with mongoose"));
+};
+
+module.exports = connectToMongo;
---
+++ Frontend/src/components/about.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import "../styles/about.css";
+
+const About = () => {
+  return (
+    <div className="container about-body">
+      <div className="about-header">About E-Mandi</div>
+
+      {/* cards */}
+      <div className="about-content">
+        <div className="about-card">
+          <div className="about-card-img sunit"></div>
+          <p className="about-card-text">
+            Name: Sunit Pal <br /> Enroll No: 211B384
+          </p>
+        </div>
+        <div className="about-description">
+          <h3 className="about-subheader">Project Developer</h3>
+          <p>
+            I am a student at Jaypee University of Engineering and Technology,
+            Guna, and I have developed and maintained the eKrishi Bazar website.
+          </p>
+          <p>
+            eKrishi Bazar is an innovative online platform revolutionizing
+            agricultural trading. It connects farmers directly with consumers,
+            facilitating seamless transactions and eliminating middlemen.
+            Through a user-friendly interface, farmers can showcase their
+            produce, while consumers gain access to fresh, locally sourced
+            goods. eKrishi Bazar promotes fair pricing, transparency, and
+            sustainability in the agricultural supply chain.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
